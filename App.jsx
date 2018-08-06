20727df7094275693c1651efa2878fbcb888e6eb
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-08-06 22:02:19 +0530

    .

--- App.jsx
+++ App.jsx
@@ -11,14 +11,26 @@ class App extends React.Component {
    }
 }
 
-class Nav extends React.Component{
+class App1 extends React.Component{
     render(){
         return(
             <div>
+                <Header Heading="wgw4g"/>
+           </div>
+        );
+    }
+}
 
+class Header extends React.Component{
+    render(){
+        return(
+            <div class="jumbotron bg-dark text-white">
+                <h2>{this.props.Heading}</h2>
+                <a>Hello World!</a>
             </div>
         );
     }
 }
 
-export default App;
+//export default App;
+export default App1;
--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(<App Heading="React App"/>, document.getElementById('app'));
+ReactDOM.render(<App1 Heading="React App"/>, document.getElementById('app'));
