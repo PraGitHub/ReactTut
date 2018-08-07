c48535367700e38bb3e76e06d79247f8410a5c80
pradministrator <prashanthhn2509@gmail.com>
2018-08-07 09:43:31 +0530

    AddingNavigationBar

--- App.jsx
+++ App.jsx
@@ -3,15 +3,37 @@ import React from 'react';
 class App extends React.Component {
    render() {
       return (
-         <div class="jumbotron bg-dark text-white">
-            <h2>{this.props.Heading}</h2>
-            <a>Hello World!</a>
+          <div>
+            <div class="jumbotron bg-dark text-white">
+                <h2>{this.props.heading}</h2>
+                <a>Hello World!</a>
+            </div>
+            <Nav titles={['Home','Profile','Statistics','Create','Request']}/>
          </div>
       );
    }
 }
-
-class App1 extends React.Component{
+//Nav titles={data:["title":'Home',"title":'Profile',"title":'Statistics',"title":'Create',"title":'Request']}/>
+class Nav extends React.Component{
+    render(){
+        var values = this.props.titles.map(function(title, i){
+            var href = "#"+title;
+            return (
+                <li class="nav nav-item">
+                    <a class="nav-link" data-toggle="tab" href={href}>{title}</a>
+                </li>
+            );
+          });
+        return(
+            <div class="container">
+                <ul class="nav nav-tabs">
+                    {values}
+                </ul>
+            </div>
+        );
+    }
+}
+/*class App1 extends React.Component{
     render(){
         return(
             <div>
@@ -30,7 +52,8 @@ class Header extends React.Component{
             </div>
         );
     }
-}
+}*/
 
 //export default App;
-export default App1;
+export default App;
+export {Nav};
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import Nav from './App.jsx';
 
-ReactDOM.render(<App1 Heading="React App"/>, document.getElementById('app'));
+ReactDOM.render(<App heading="React App"/>, document.getElementById('app'));
+//ReactDOM.render(<Nav titles={['Home','Profile','Statistics','Create','Request']}/>,document.getElementById('nav'));
