d904f5b4ed71612071047265ef16829b4df37c49
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2019-12-01 13:39:25 +0530

    react tut

--- README.md
+++
@@ -1,4 +0,0 @@
-# ReactTut
-React tutorial
-
-nodejs|react
--- main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import Nav from './App.jsx';
-
-ReactDOM.render(<App heading="React App" titles={['Home','Profile','Statistics','Create','Request']}/>, document.getElementById('app'));
-//ReactDOM.render(<Nav titles={['Home','Profile','Statistics','Create','Request']}/>,document.getElementById('nav'));
